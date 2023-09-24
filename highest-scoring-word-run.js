ebee098ba20c7029f8045db6bf0e9353e927e6fe
Utku Ergün <111204328+utku0640@users.noreply.github.com>
2023-09-24 15:49:25 +0300

    v1
    v1

--- highest-scoring-word-run.js
+++ highest-scoring-word-run.js
@@ -1,5 +1,5 @@
-const highestScoringWord = require('./highest-scoring-word');
+const highestScoringWord = require("./highest-scoring-word");
 
-const result = highestScoringWord('Hello my name is xavier');
+const result = highestScoringWord("Hello my name is xavier");
 
 console.log(result);
--- highest-scoring-word.js
+++ highest-scoring-word.js
@@ -1,3 +1,22 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const formatted = str.toLowerCase().split(" ");
+  let wordArray = [];
+  for (let i = 0; i < formatted.length; i++) {
+    wordArray.push(formatted[i]);
+  }
+  const countObject = {};
+  let score = 0;
+  for (const word of wordArray) {
+    for (let i = 0; i < word.length; i++) {
+      score += word[i].charCodeAt() - 96;
+      countObject[word] = score;
+    }
+    score = 0;
+  }
+  const allScore = Object.values(countObject);
+  const maxNumber = Math.max(...allScore);
+  console.log(countObject);
+  return Object.keys(countObject).find((key) => countObject[key] === maxNumber);
+}
 
 module.exports = highestScoringWord;
